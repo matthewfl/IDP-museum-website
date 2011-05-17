6c678a4f69ec54db7ddfde5106942e27c4c907b5
Matthew Francis-Landau <matthew@matthewfl.com>
2011-05-17 00:13:57 -0700

    fixed scolling of fixed background on tablet

--- static/code.js
+++ static/code.js
@@ -1,3 +1,4 @@
+var tablet = true;
 var socket =  new io.Socket();/*(null, {transportOptions: {
     'xhr-polling': {
 	timeout: 3000
@@ -18,11 +19,24 @@ $(function () {
 });
 
 
+
 $(function () {
     $("a").live('click', function () { 
 	var href = $(this).attr("href");
 	if(/http/.exec(href)) { alert("This open external web page: "+href); return false; }
 	$("#content").load(href);
+	$(document).scrollTop(0);
 	return false;
     });
+
+//    if($(window).height()*$(window).width() > 400000)	tablet = false;
+
+    if(tablet) {
+	alert("tablet");
+	var base_scroll = $("#base").css("position", "absolute");
+	$(window).scroll(function () {
+	    base_scroll.css("top", document.body.scrollTop);
+	});
+	$(document).scrollTop(0);
+    }
 });
