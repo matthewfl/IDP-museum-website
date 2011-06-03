2e40cdecfffab86031126543d173ebf34b2352b0
Matthew Francis-Landau <matthew@matthewfl.com>
2011-06-03 08:35:43 -0700

    final mess

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var router = require('./lib/node-router');
 var server = router.getServer();
 var io = require('./lib/socket.io');
 var socket = io.listen(server.getRaw());
+var fs = require('fs');
+
+var aliveCount = 0;
 
 server.get('/', router.staticHandler('./pages/index.html'));
 
-server.get('/ping', function (req, res) {
-    socket.broadcast("test");
+server.get('/reload', function (req, res) {
+    socket.broadcast("reload");
+    return "sent";
+});
+
+server.get('/qtest', function () {
+    runQuestion();
     return "sent";
 });
 
@@ -17,8 +25,83 @@ server.get(/\/random(.*)/, function (req, res, match) {
 server.get(/\/static/, router.staticDirHandler('./static/', '/static'));
 server.get(/\/pages/, router.staticDirHandler('./pages/', '/pages'));
 
+server.get(/\/ping/, function (req, res) {
+    res.writeHead(200);
+    setTimeout(function () {
+	res.write("document.write('<script src=\"/ping"+Math.random()+"\"></script>');");
+	res.end()
+    }, 5000);
+});
+
+var questions = eval('(' + fs.readFileSync('./questions.dat').toString() + ')');
+
+var current_question = [];
+var question_queue = [];
+var question_html = false;
+
+function formatQuestion (question) {
+    var str='<div id="question"><center><h1>Terradise</h1></center>';
+    str += 
+    str += '<p>'+question[0]+'</p>';
+    str += '<br><ol>'
+    for(var i=2; i<question.length;i++) {
+	str += "<li><a href=\"#question"+Math.random()+"\" class=\"answer"+(i==question[1]+2?" correct":"")+"\">"+question[i]+"</a></li>";
+    }
+    str += "</ol><div id='qthanks'></div>"
+    return str;
+}
+
+server.get('/question', function (req, res) {
+    if(question_html) return question_html;
+    question_queue.push(res);
+});
+
+var question_running = false;
+function  runQuestion () {
+    return;
+    if(question_running) return;
+    question_html = false;
+    socket.broadcast("quiz");
+    setTimeout(function () {
+	var q = questions[Math.floor(Math.random()*questions.length)];
+	question_html = formatQuestion(q);
+	for(var i=0; i < question_queue.length; i++) {
+	    try {
+		question_queue[i].write(question_html);
+		question_queue[i].end();
+
+	    } catch(e) {}
+	}
+	setTimeout(function () {
+	    socket.broadcast("showA");
+	    setTimeout(function () {
+		question_running = false;
+	    }, 50000); // 50 sec
+	}, 15000);
+    }, 2500);
+    question_running = true;
+}
+
 server.listen(8080);
 
 socket.on('connection', function (client) {
-    
+    client.on('message', function (data) {
+	if(data == "alive") {
+	    aliveCount++;
+	    console.log("alive count", aliveCount, "of", socket.server.connections);
+	    setTimeout(function() {
+		aliveCount--;
+	    }, 2970);
+	}
+
+	if(aliveCount >= 3) {
+	    if(Math.random() > .2) // fix to higher number
+		runQuestion();
+	}
+    });
+    client.on('disconnect', function () {
+
+    });
 });
+
+console.log(socket)
--- static/code.js
+++ static/code.js
@@ -1,26 +1,16 @@
 var tablet = true;
-var socket =  new io.Socket();/*(null, {transportOptions: {
-				'xhr-polling': {
-				timeout: 3000
-				},
-				'jsonp-polling': {
-				timeout: 3000
-				}
-				}});*/
-socket.on('disconnect', function () {
-    //alert("server lost");
-    $("body").append("<p>lost</p>");
+var socket =  new io.Socket(document.domain, {
+    connectTimeout: 6000,
+    transports: ['xhr-multipart', 'xhr-polling', 'jsonp-polling'],
+    reconnectionDelay: 25,
+    maxReconnectionAttempts: 100
 });
-socket.on('message', function (data) {
-    // alert(data);
-});
-$(function () {
-    socket.connect();
-});
+
 
 
 function link (href) {
     //var href = $(this).attr("href");
+    if(/question/.exec(href)) return false;
     if(/http/.exec(href)) { alert("This opens external web page: "+href); return false; }
     $("#contentParent").load(href + " #content");
     var background = /pages\/.*\.html/.exec(href) ? href.replace(/\/pages\/(.*)\.html/, "/static/backgrounds/$1.jpg") : "/static/backgrounds/default.jpg";
@@ -31,20 +21,23 @@ function link (href) {
 
 $(function () {
     $("a").live('click', function () {
+	alive = true;
 	return link($(this).attr("href"));
     });
 
-    if($(window).height()*$(window).width() > 400000)	tablet = false;
-
+    if($(window).height()*$(window).width() > 400000) tablet = false;
+    
     if(tablet) {
 	//	alert("tablet");
 	var base_scroll = $("#base").css("position", "absolute");
 	$(window).scroll(function () {
 	    base_scroll.css("top", document.body.scrollTop);
+	    alive = true;
 	});
 	$(document).scrollTop(0);
     }else{
 	$("#background").css("background", 'url("/static/backgrounds/default.jpg") 0px -32px');
+	$("#nav").css("top", "0px");
 	//$("#nav").css("background", '#404040');
     }
 });
@@ -53,3 +46,53 @@ $(function () {
 for(var i=0;i<20;i++)
     location.hash=i;
 location.hash="";
+
+
+var alive = false;
+$(window).mousemove(function () {
+    alive = true;
+});
+
+setInterval(function () {
+    if(alive) socket.send("alive");
+}, 3000);
+
+setInterval(function () {
+    alive = false;
+}, 20000);
+
+
+socket.on('disconnect', function () {
+    setTimeout(function () {
+	socket.connect();
+    }, 2500);
+});
+socket.on('message', function (data) {
+    if(data == "reload") location.reload(true);
+    if(data == "quiz") $.facebox({ajax: "/question"});
+    if(data == "showA") {
+	$("a.answer").parent().css("background", "#FF0000");
+	$("a.answer.correct").parent().css("background", "#00FF00");
+	$("#qthanks").html("<h3>Thank you for participating in the Terradise quiz<br>We hope that you enjoyed the Museum</h3>");
+	setTimeout(function () {
+	    $(document).trigger('close.facebox');
+	}, 5000);
+    }
+});
+
+$(function () {
+    socket.connect();
+});
+
+$("a.answer").live('click', function () {
+    $("a.answer").parent().css("font-weight", "normal");
+    $(this).parent().css("font-weight", "bold");
+    return false;
+});
+
+/*
+setInterval(function () {
+    $.ajax('/ping'+Math.random(), function () {});
+}, 5000);
+
+*/
